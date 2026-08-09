afe5f241430d6c6965fa6ac777efc6ecaea5e6d8
gdamms <damguillotin@gmail.com>
2026-08-09 10:40:25 +0200

    archive frame in tempfile first

--- frame/scripts/archive.sh
+++ frame/scripts/archive.sh
@@ -6,7 +6,10 @@ SCRIPT_DIR=$(CDPATH= cd -- "$(dirname "$0")" && pwd)
 FRAMILY_ARCHIVE_DIR="$SCRIPT_DIR/.."
 FRAMILY_ARCHIVE_FILE="$FRAMILY_ARCHIVE_DIR/framily.tar.gz"
 
-tar -czf "$FRAMILY_ARCHIVE_FILE" \
+TMP_ARCHIVE_FILE=$(mktemp)
+trap 'rm -f "$TMP_ARCHIVE_FILE"' EXIT
+
+tar -czf "$TMP_ARCHIVE_FILE" \
     -C "$FRAMILY_ARCHIVE_DIR" \
     --exclude="framily.tar.gz" \
     --exclude=".git" \
@@ -18,3 +21,5 @@ tar -czf "$FRAMILY_ARCHIVE_FILE" \
     --exclude="agent.lock" \
     --exclude="agent.recheck" \
     .
+
+mv "$TMP_ARCHIVE_FILE" "$FRAMILY_ARCHIVE_FILE"
